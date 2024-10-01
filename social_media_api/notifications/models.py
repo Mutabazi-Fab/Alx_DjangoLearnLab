21c976d73b1b4740be269864f317bfa534dd47eb
Fabrices-MacBook <fmutabazi9@gmail.com>
2024-10-01 15:03:57 +0200

    django proj

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,35 +1,17 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# Create your models here.
+#Notification Model
+class Notification(models.Model):
+    recipient = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    actor = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='actor')
+    verb = models.CharField(max_length=255)
+    content_type = models.ForeignKey('contenttypes.ContentType', on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('content_type', 'object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'Comment by {self.author.username} on {self.post.title}'
-    
-class Like(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        # Ensures a user can only like a post once
-        unique_together = ('user', 'post')  
-        
-    def __str__(self):
-        return f"{self.user.username} likes {self.post.title}"
-    
+def __str__(self):
+        return self.verb
